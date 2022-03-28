a0b87edef559fb0c1e37f0ef61c41c569ac8f5a7
root <abdulsemedabdelila65@gmail.com>
2022-03-28 03:34:58 -0700

    commit changes

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,15 +1,12 @@
-/*
- * File: 100-set_string.c
- * Auth: Brennan D Baraban
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
 
-#include "main.h"
-#include "stdio.h"
-/**
- * set_string - Sets the value of a pointer to a char.
- * @s: The pointer.
- * @to: The char.
- */
 void set_string(char **s, char *to)
 {
 	*s = to;
