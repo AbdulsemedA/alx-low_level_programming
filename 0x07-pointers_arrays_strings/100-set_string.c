ad44340515bdd05bae153feeb17f037b6839fb2f
root <abdulsemedabdelila65@gmail.com>
2022-03-28 04:31:29 -0700

    commit

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-#include "main.h"
-
-/**
- * set_string - sets the value of a pointer to a char
- * @s: content is to be copied
- * @to: string
- *
- * Return: void
- */
-void set_string(char **s, char *to)
-{
-	*s = to;
-}
-=======
 #include "main.h"
 /**
   *set_string - sets the value of a pointer to a char.
   *@s: pointer to pointer.
-  *@to: pointer to char.
+  *@to: pointer to char
   *
   *Return: void.
   */
@@ -25,4 +10,3 @@ void set_string(char **s, char *to)
 {
 	*s = to;
 }
->>>>>>> a42646568e4cb2d4fb72346c0b7c11bff4466fc5
